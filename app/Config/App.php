676c16eae5674471ada519c67263ca20434d8ee3
Angel Knutsen Aune <djangel@live.no>
2025-02-08 16:00:55 +0100

    Fixes and Cleanups in files

--- app/Config/App.php
+++ app/Config/App.php
@@ -6,197 +6,153 @@ use CodeIgniter\Config\BaseConfig;
 
 class App extends BaseConfig
 {
-    /**
-     * --------------------------------------------------------------------------
-     * Base Site URL
-     * --------------------------------------------------------------------------
-     *
-     * URL to your CodeIgniter root. Typically, this will be your base URL,
-     * WITH a trailing slash:
-     *
-     * E.g., http://example.com/
-     */
-    public string $baseURL = 'http://localhost:8080/';
-
-    /**
-     * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
-     * If you want to accept multiple Hostnames, set this.
-     *
-     * E.g.,
-     * When your site URL ($baseURL) is 'http://example.com/', and your site
-     * also accepts 'http://media.example.com/' and 'http://accounts.example.com/':
-     *     ['media.example.com', 'accounts.example.com']
-     *
-     * @var list<string>
-     */
-    public array $allowedHostnames = [];
-
-    /**
-     * --------------------------------------------------------------------------
-     * Index File
-     * --------------------------------------------------------------------------
-     *
-     * Typically, this will be your `index.php` file, unless you've renamed it to
-     * something else. If you have configured your web server to remove this file
-     * from your site URIs, set this variable to an empty string.
-     */
-    public string $indexPage = 'index.php';
-
-    /**
-     * --------------------------------------------------------------------------
-     * URI PROTOCOL
-     * --------------------------------------------------------------------------
-     *
-     * This item determines which server global should be used to retrieve the
-     * URI string. The default setting of 'REQUEST_URI' works for most servers.
-     * If your links do not seem to work, try one of the other delicious flavors:
-     *
-     *  'REQUEST_URI': Uses $_SERVER['REQUEST_URI']
-     * 'QUERY_STRING': Uses $_SERVER['QUERY_STRING']
-     *    'PATH_INFO': Uses $_SERVER['PATH_INFO']
-     *
-     * WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
-     */
-    public string $uriProtocol = 'REQUEST_URI';
+    /*
+    |--------------------------------------------------------------------------
+    | Base Site URL
+    |--------------------------------------------------------------------------
+    |
+    | The URL to your CodeIgniter root. Typically, this will be your base URL,
+    | WITH a trailing slash:
+    |
+    |   'http://example.com/'
+    |
+    */
+    public string $baseURL = 'https://docs.core-x.dev/';
+    /*
+    |--------------------------------------------------------------------------
+    | Index File
+    |--------------------------------------------------------------------------
+    |
+    | Typically, this will be "index.php". However, if you are using mod_rewrite
+    | to remove the page set this variable so that it is blank.
+    |
+    */
+    public string $indexPage = '';
+    /*
+    |--------------------------------------------------------------------------
+    | Default Locale
+    |--------------------------------------------------------------------------
+    |
+    | The default language/locale of your application. Must be in a valid format.
+    |
+    */
+    public string $defaultLocale = 'en';
 
     /*
     |--------------------------------------------------------------------------
-    | Allowed URL Characters
+    | Supported Locales
     |--------------------------------------------------------------------------
     |
-    | This lets you specify which characters are permitted within your URLs.
-    | When someone tries to submit a URL with disallowed characters they will
-    | get a warning message.
-    |
-    | As a security measure you are STRONGLY encouraged to restrict URLs to
-    | as few characters as possible.
-    |
-    | By default, only these are allowed: `a-z 0-9~%.:_-`
-    |
-    | Set an empty string to allow all characters -- but only if you are insane.
-    |
-    | The configured value is actually a regular expression character group
-    | and it will be used as: '/\A[<permittedURIChars>]+\z/iu'
-    |
-    | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
+    | Array of supported locales.
     |
     */
-    public string $permittedURIChars = 'a-z 0-9~%.:_\-';
-
-    /**
-     * --------------------------------------------------------------------------
-     * Default Locale
-     * --------------------------------------------------------------------------
-     *
-     * The Locale roughly represents the language and location that your visitor
-     * is viewing the site from. It affects the language strings and other
-     * strings (like currency markers, numbers, etc), that your program
-     * should run under for this request.
-     */
-    public string $defaultLocale = 'en';
-
-    /**
-     * --------------------------------------------------------------------------
-     * Negotiate Locale
-     * --------------------------------------------------------------------------
-     *
-     * If true, the current Request object will automatically determine the
-     * language to use based on the value of the Accept-Language header.
-     *
-     * If false, no automatic detection will be performed.
-     */
-    public bool $negotiateLocale = false;
-
-    /**
-     * --------------------------------------------------------------------------
-     * Supported Locales
-     * --------------------------------------------------------------------------
-     *
-     * If $negotiateLocale is true, this array lists the locales supported
-     * by the application in descending order of priority. If no match is
-     * found, the first locale will be used.
-     *
-     * IncomingRequest::setLocale() also uses this list.
-     *
-     * @var list<string>
-     */
     public array $supportedLocales = ['en'];
 
-    /**
-     * --------------------------------------------------------------------------
-     * Application Timezone
-     * --------------------------------------------------------------------------
-     *
-     * The default timezone that will be used in your application to display
-     * dates with the date helper, and can be retrieved through app_timezone()
-     *
-     * @see https://www.php.net/manual/en/timezones.php for list of timezones
-     *      supported by PHP.
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | This setting affects how dates are displayed and parsed. Default is UTC.
+    |
+    */
     public string $appTimezone = 'UTC';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Default Character Set
-     * --------------------------------------------------------------------------
-     *
-     * This determines which character set is used by default in various methods
-     * that require a character set to be provided.
-     *
-     * @see http://php.net/htmlspecialchars for a list of supported charsets.
-     */
-    public string $charset = 'UTF-8';
-
-    /**
-     * --------------------------------------------------------------------------
-     * Force Global Secure Requests
-     * --------------------------------------------------------------------------
-     *
-     * If true, this will force every request made to this application to be
-     * made via a secure connection (HTTPS). If the incoming request is not
-     * secure, the user will be redirected to a secure version of the page
-     * and the HTTP Strict Transport Security (HSTS) header will be set.
-     */
-    public bool $forceGlobalSecureRequests = false;
-
-    /**
-     * --------------------------------------------------------------------------
-     * Reverse Proxy IPs
-     * --------------------------------------------------------------------------
-     *
-     * If your server is behind a reverse proxy, you must whitelist the proxy
-     * IP addresses from which CodeIgniter should trust headers such as
-     * X-Forwarded-For or Client-IP in order to properly identify
-     * the visitor's IP address.
-     *
-     * You need to set a proxy IP address or IP address with subnets and
-     * the HTTP header for the client IP address.
-     *
-     * Here are some examples:
-     *     [
-     *         '10.0.1.200'     => 'X-Forwarded-For',
-     *         '192.168.5.0/24' => 'X-Real-IP',
-     *     ]
-     *
-     * @var array<string, string>
-     */
-    public array $proxyIPs = [];
-
-    /**
-     * --------------------------------------------------------------------------
-     * Content Security Policy
-     * --------------------------------------------------------------------------
-     *
-     * Enables the Response's Content Secure Policy to restrict the sources that
-     * can be used for images, scripts, CSS files, audio, video, etc. If enabled,
-     * the Response object will populate default values for the policy from the
-     * `ContentSecurityPolicy.php` file. Controllers can always add to those
-     * restrictions at run time.
-     *
-     * For a better understanding of CSP, see these documents:
-     *
-     * @see http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-     * @see http://www.w3.org/TR/CSP/
-     */
-    public bool $CSPEnabled = false;
+    /*
+    |--------------------------------------------------------------------------
+    | Application Namespace
+    |--------------------------------------------------------------------------
+    |
+    | This defines the default Namespace that all application classes should be
+    | in.
+    |
+    */
+    public string $appNamespace = 'App';
+    /*
+    |--------------------------------------------------------------------------
+    | Encryption Key
+    |--------------------------------------------------------------------------
+    |
+    | The encryption key used for handling sensitive data. Ensure this is set.
+    |
+    */
+    public string $encryptionKey = '';
+    /*
+    |--------------------------------------------------------------------------
+    | CSRF Protection
+    |--------------------------------------------------------------------------
+    |
+    | Cross-Site Request Forgery protection settings.
+    |
+    */
+    public bool $CSRFProtect = true;
+    public ?int $CSRFExpire = 7200;
+    public string $CSRFTokenName = 'csrf_token';
+    public string $CSRFCookieName = 'csrf_cookie';
+    public bool $CSRFRedirect = true;
+    /*
+    |--------------------------------------------------------------------------
+    | Content Security Policy (CSP)
+    |--------------------------------------------------------------------------
+    |
+    | Define the security policies for the frontend.
+    |
+    */
+    public array $CSPEnabled = [];
+    /*
+    |--------------------------------------------------------------------------
+    | Session Settings
+    |--------------------------------------------------------------------------
+    |
+    | Default session driver and settings for the application.
+    |
+    */
+    public string $sessionDriver = 'CodeIgniter\Session\Handlers\FileHandler';
+    public string $sessionSavePath = WRITEPATH . 'session';
+    public string $sessionCookieName = 'corex_session';
+    public bool $sessionMatchIP = false;
+    public int $sessionExpiration = 7200;
+    public bool $sessionRegenerateDestroy = false;
+    /*
+    |--------------------------------------------------------------------------
+    | Cookie Settings
+    |--------------------------------------------------------------------------
+    |
+    | Settings for cookie handling in the application.
+    |
+    */
+    public string $cookiePrefix = '';
+    public string $cookieDomain = '';
+    public string $cookiePath = '/';
+    public string $cookieSecure = 'auto';
+    public bool $cookieHTTPOnly = true;
+    /*
+    |--------------------------------------------------------------------------
+    | Logging & Debugging
+    |--------------------------------------------------------------------------
+    |
+    | Application debugging settings.
+    |
+    */
+    public bool $appDebug = false;
+    public string $logThreshold = '4'; // Log All Errors
+    public string $logPath = '';
+    /*
+    |--------------------------------------------------------------------------
+    | Allowed Hostnames
+    |--------------------------------------------------------------------------
+    |
+    | Set allowed hostnames for security purposes.
+    |
+    */
+    public array $allowedHosts = ['docs.core-x.dev'];
+    /*
+    |--------------------------------------------------------------------------
+    | Error Handling
+    |--------------------------------------------------------------------------
+    |
+    | Error display settings in production.
+    |
+    */
+    public bool $displayErrors = false;
 }
--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -4,91 +4,51 @@ namespace Config;
 
 use CodeIgniter\Config\AutoloadConfig;
 
-/**
- * -------------------------------------------------------------------
- * AUTOLOADER CONFIGURATION
- * -------------------------------------------------------------------
- *
- * This file defines the namespaces and class maps so the Autoloader
- * can find the files as needed.
- *
- * NOTE: If you use an identical key in $psr4 or $classmap, then
- *       the values in this file will overwrite the framework's values.
- *
- * NOTE: This class is required prior to Autoloader instantiation,
- *       and does not extend BaseConfig.
- *
- * @immutable
- */
 class Autoload extends AutoloadConfig
 {
+    /*
+    |--------------------------------------------------------------------------
+    | Auto-Loader Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The AutoloadConfig class allows you to define the locations of Namespaces,
+    | PSR-4 autoloading, classmap files, and helper functions that should be
+    | loaded automatically when CodeIgniter starts.
+    |
+    */
     /**
-     * -------------------------------------------------------------------
-     * Namespaces
-     * -------------------------------------------------------------------
-     * This maps the locations of any namespaces in your application to
-     * their location on the file system. These are used by the autoloader
-     * to locate files the first time they have been instantiated.
+     * Maps namespaces to their locations within the application.
+     * These are used when Autoloading classes using PSR-4.
      *
-     * The 'Config' (APPPATH . 'Config') and 'CodeIgniter' (SYSTEMPATH) are
-     * already mapped for you.
-     *
-     * You may change the name of the 'App' namespace if you wish,
-     * but this should be done prior to creating any namespaced classes,
-     * else you will need to modify all of those classes for this to work.
-     *
-     * @var array<string, list<string>|string>
+     * @var array
      */
-    public $psr4 = [
-        APP_NAMESPACE => APPPATH,
+    public array $psr4 = [
+        'App'          => APPPATH,         // The main application namespace
+        'Config'       => APPPATH . 'Config',
+        'CoreX\\Docs'  => APPPATH,         // Namespace for CoreX-Docs components
     ];
 
     /**
-     * -------------------------------------------------------------------
-     * Class Map
-     * -------------------------------------------------------------------
-     * The class map provides a map of class names and their exact
-     * location on the drive. Classes loaded in this manner will have
-     * slightly faster performance because they will not have to be
-     * searched for within one or more directories as they would if they
-     * were being autoloaded through a namespace.
+     * Classmap provides a map of class names and their exact file locations.
+     * Used for files that are not PSR-4 compliant.
      *
-     * Prototype:
-     *   $classmap = [
-     *       'MyClass'   => '/path/to/class/file.php'
-     *   ];
-     *
-     * @var array<string, string>
+     * @var array
      */
-    public $classmap = [];
+    public array $classmap = [];
 
     /**
-     * -------------------------------------------------------------------
-     * Files
-     * -------------------------------------------------------------------
-     * The files array provides a list of paths to __non-class__ files
-     * that will be autoloaded. This can be useful for bootstrap operations
-     * or for loading functions.
+     * Files to be automatically loaded on every request.
+     * Used for including custom global functions, helper files, or utilities.
      *
-     * Prototype:
-     *   $files = [
-     *       '/path/to/my/file.php',
-     *   ];
-     *
-     * @var list<string>
+     * @var array
      */
-    public $files = [];
+    public array $files = [];
 
     /**
-     * -------------------------------------------------------------------
-     * Helpers
-     * -------------------------------------------------------------------
-     * Prototype:
-     *   $helpers = [
-     *       'form',
-     *   ];
+     * Helper files to be automatically loaded.
      *
-     * @var list<string>
+     * @var array
      */
-    public $helpers = [];
+    public array $helpers = ['url', 'form', 'text', 'security'];
+
 }
